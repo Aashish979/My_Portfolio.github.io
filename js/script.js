8fdbef721b301687b319a4de3a0fb0df90ec7e58
Ashish Gupta <onlineashish083@gmail.com>
2025-05-27 11:04:10 +0530

    Fixed version of My Portfolio

--- js/script.js
+++ js/script.js
@@ -1,60 +1,52 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  // Toggle menu icon and header
+  $('#menu').click(function () {
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
 
-	$('#menu').click(function(){
-		$(this).toggleClass('fa-times');
-		$('header').toggleClass('toggle');
-	});
+  // Scroll events
+  $(window).on('scroll load', function () {
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
 
-$(window).on('scroll load', function(){
-	$('#menu').removeClass('fa-times');
-	$('header').removeClass('toggle');
-
-	if($(window).scrollTop()>0){
-		$('.top').show();
-	}
-	else{
-		$('.top').hide();
-	}
-	
-});
-
-
-//smooth scrolling
-
-$('a[href*="#"]').on('click',function(e){
-
-		e.preventDefault();
-
-		$('html, body').animate({
-				scrollTop : $($(this).attr('href')).offset().top,
-		},
-			500,
-			'linear'
-		);
-});
-
-$('.owl-carousel').owlCarousel({
-    // loop:true,
-    margin:30,
-    nav:true,
-    // autoplay:true
-    responsive:{
-        0:{
-            items:1
-            // nav:true
-        },
-        600:{
-            items:4
-            // nav:true
-        },
-
-        1000:{
-            items:4
-            // nav:true
-          
-        }
-
+    if ($(this).scrollTop() > 0) {
+      $('.top').fadeIn(); // Smooth appearance
+    } else {
+      $('.top').fadeOut(); // Smooth hide
     }
-})
+  });
 
+  // Smooth scrolling
+  $('a[href^="#"]').on('click', function (e) {
+    const target = $(this.hash);
+
+    if (target.length) {
+      e.preventDefault();
+      $('html, body').animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        500,
+        'linear'
+      );
+    }
+  });
+
+  // Owl Carousel init
+  $('.owl-carousel').owlCarousel({
+    margin: 30,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 });
